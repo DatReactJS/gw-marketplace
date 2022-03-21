25f33ec172cb2f04aa848f1a15d9408d5fdfb174
freshervn <dat.bt@hesperiafreepress.com>
2022-03-21 10:38:22 +0700

    new brand

--- src/pages/Home/Footer/index.tsx
+++ src/pages/Home/Footer/index.tsx
@@ -4,25 +4,27 @@ import Icon from '@/components/Icon';
 function Footer() {
   return (
     <>
-      <div></div>
-      <div className={styles.social}>
-        <a href="http://" target="_blank" rel="noopener noreferrer">
-          <Icon icon="path14" size={24} color="#1AD7FD"></Icon>
-        </a>
-        <a href="http://" target="_blank" rel="noopener noreferrer">
-          <Icon icon="YouTube-Icon" size={24} color="#1AD7FD"></Icon>
-        </a>
-        <a href="http://" target="_blank" rel="noopener noreferrer">
-          <Icon icon="Vector-2" size={24} color="#1AD7FD"></Icon>
-        </a>
-        <a href="http://" target="_blank" rel="noopener noreferrer">
-          <Icon icon="Group" size={24} color="#1AD7FD"></Icon>
-        </a>
-        <a href="http://" target="_blank" rel="noopener noreferrer">
-          <Icon icon="Vector-4" size={24} color="#1AD7FD"></Icon>
-        </a>
+      <div className={styles.footer}>
+        <hr />
+        <div className={styles.social}>
+          <a href="http://" target="_blank" rel="noopener noreferrer">
+            <Icon icon="path14" size={24} color="#1AD7FD"></Icon>
+          </a>
+          <a href="http://" target="_blank" rel="noopener noreferrer">
+            <Icon icon="YouTube-Icon" size={24} color="#1AD7FD"></Icon>
+          </a>
+          <a href="http://" target="_blank" rel="noopener noreferrer">
+            <Icon icon="Vector-2" size={24} color="#1AD7FD"></Icon>
+          </a>
+          <a href="http://" target="_blank" rel="noopener noreferrer">
+            <Icon icon="Group" size={24} color="#1AD7FD"></Icon>
+          </a>
+          <a href="http://" target="_blank" rel="noopener noreferrer">
+            <Icon icon="Vector-4" size={24} color="#1AD7FD"></Icon>
+          </a>
+        </div>
+        <h5>Copyright 2021 galactic war</h5>
       </div>
-      <h5>Copyright 2021 galactic war</h5>
     </>
   );
 }
